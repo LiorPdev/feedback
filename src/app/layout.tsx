d1586128b35e7cb32afebf8022d514a6642cedd9
Lior Porat <lior@example.com>
2026-03-19 13:42:43 +0200

    fix12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,6 +83,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       localization={heIL as any}
       appearance={{
         elements: {
